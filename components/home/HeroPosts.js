8f90fb2f2d182fd669bbfa8057aa03820318488b
Murat Özdemir <mrtozdmr92@gmail.com>
2021-12-19 10:43:11 +0300

    Solved Key

--- components/home/HeroPosts.js
+++ components/home/HeroPosts.js
@@ -4,7 +4,7 @@ const HeroPosts = ({ posts }) => {
   return (
     <div className="container grid gap-4 px-4 mx-auto mt-8 mb-32 lg:gap-8 md:grid-cols-2 lg:grid-cols-3">
       {posts.map((post) => (
-        <HeroPostItem post={post} key={post.postId} />
+        <HeroPostItem post={post} key={post.data.slug} />
       ))}
     </div>
   );
